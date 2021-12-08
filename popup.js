93a00d20908c296bc5636a69cb958c7e08e567ee
Prajwal Jaiswal <prajwal.jaiswal@hotmail.com>
2021-12-08 10:21:56 +0530

    Connect to Flask backend

--- popup.js
+++ popup.js
@@ -1,5 +1,29 @@
-chrome.tabs.executeScript( {
-  code: "window.getSelection().toString();"
-}, function(selection) {
-  document.querySelector("textarea").innerHTML = selection[0];
+chrome.tabs.executeScript(
+  {
+    code: 'window.getSelection().toString();',
+  },
+  function (selection) {
+    document.querySelector('#inputText').innerHTML = selection[0];
+  }
+);
+
+let translateButton = document.querySelector('button');
+let inputText = document.querySelector('#inputText');
+
+translateButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  console.log('button clicked');
+
+  let url = 'http://localhost:5000/translate?text=' + inputText.textContent;
+  fetch(url)
+    // Converting received data to JSON
+    .then((response) => response.json())
+    .then((json) => {
+      console.log(json);
+
+      // Display result
+      document.querySelector('#outTitle').innerHTML = 'Output -';
+      document.querySelector('#devanagari').innerHTML = json.hindi_text;
+      document.querySelector('#english').innerHTML = json.eng_text;
+    });
 });
